4df06d4aefa2c76bcda9a01536d1b0ee6e2db5eb
kychoiyohoho <mjnhy2001@naver.com>
2022-07-02 21:45:48 +0900

    tailwindcss 맛보기

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,tx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
